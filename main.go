6a61f251fd3aaf3e6811438915f913e0a8a047c9
David Bierce <david@bierce.org>
2016-07-21 03:54:47 -0500

    Implement padding.  Only support "0" for pad and only right padding.

--- main.go
+++ main.go
@@ -7,3 +7,7 @@ import (
 func leftPad(paddingLength int, stringToPad string) string {
 	return strings.Repeat("0",paddingLength)+stringToPad
 }
+
+func rightPad(paddingLength int, stringToPad string) string {
+	return stringToPad+strings.Repeat("0",paddingLength)
+}
--- main_test.go
+++ main_test.go
@@ -2,9 +2,18 @@ package Padding_Service
 
 import "testing"
 
-func TestLeftPad_Function_StringReturn(t *testing.T) {
+func TestLeftPad_Function_Returns_padded_String(t *testing.T) {
 	result := leftPad(3, "Howdy")
 	expect := "000Howdy"
+
+	if result != expect {
+		t.Errorf("Test Failed, expected: %s but got %s.", expect, result)
+	}
+}
+
+func TestRightPad_Function_StringReturn(t *testing.T) {
+	result := rightPad(3, "Howdy")
+	expect := "Howdy000"
 
 	if result != expect {
 		t.Errorf("Test Failed, expected: %s but got %s.", expect, result)
